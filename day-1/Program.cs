365d4fa423f08a23b61806e417408262e61f7e85
Alex Krupnov <akrupnov@purestorage.com>
2020-12-02 22:00:55 +0100

    Day 1, Task 2

--- day-1/Program.cs
+++ day-1/Program.cs
@@ -11,11 +11,22 @@ namespace day_1
             var input = File.ReadAllLines("input");
             var expenseReport = input.Select(x => Convert.ToInt32(x)).ToList();
 
-            var allPairs = expenseReport.SelectMany(x => expenseReport, (x, y) => Tuple.Create(x, y)).Where(tuple => tuple.Item1 != tuple.Item2).ToList();
+            var allPairs = expenseReport.
+                                SelectMany(x => expenseReport, (x, y) => Tuple.Create(x, y)).
+                                Where(tuple => tuple.Item1 != tuple.Item2).ToList();
+            
+            var allTriplets = expenseReport.SelectMany(a => expenseReport.Where(b => (b > a)).
+                                    SelectMany(b => expenseReport.Where(c => (c > b)).
+                                    Select(c => Tuple.Create(a, b, c)))).ToList();
 
-            var result = allPairs.Where(t => t.Item1 + t.Item2 == 2020).Select(t => t.Item1 * t.Item2).First();
 
-            Console.WriteLine($"{result}");
+            var firstResult = allPairs.Where(t => t.Item1 + t.Item2 == 2020).Select(t => t.Item1 * t.Item2).First();
+
+            Console.WriteLine($"{firstResult}");
+            
+            var secondResult = allTriplets.Where(t => t.Item1 + t.Item2 + t.Item3 == 2020).Select(t => t.Item1*t.Item2*t.Item3).First();
+
+            Console.WriteLine($"{secondResult}");
 
             Console.ReadLine();
         }

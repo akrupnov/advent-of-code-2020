738fd838c30311e5447486ada919a531b6b4cf91
Alex Krupnov <akrupnov@purestorage.com>
2020-12-02 21:40:44 +0100

    Day 1, Task 1

---
+++ day-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input");
+            var expenseReport = input.Select(x => Convert.ToInt32(x)).ToList();
+
+            var allPairs = expenseReport.SelectMany(x => expenseReport, (x, y) => Tuple.Create(x, y)).Where(tuple => tuple.Item1 != tuple.Item2).ToList();
+
+            var result = allPairs.Where(t => t.Item1 + t.Item2 == 2020).Select(t => t.Item1 * t.Item2).First();
+
+            Console.WriteLine($"{result}");
+
+            Console.ReadLine();
+        }
+    }
+}
